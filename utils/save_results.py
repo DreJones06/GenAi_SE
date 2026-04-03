c18550d8dd9e6f6925a4e7801d16bf48607425f8
DreJones06 <157946805+DreJones06@users.noreply.github.com>
2026-04-03 10:28:40 +0530

    Delete utils directory

--- utils/save_results.py
+++
@@ -1,9 +0,0 @@
-# utils/save_results.py
-
-def save_to_file(data):
-    file_path = "data/results.txt"
-
-    with open(file_path, "w", encoding="utf-8") as f:
-        f.write(data)
-
-    print(f"📁 Results saved to {file_path}")
